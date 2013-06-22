2cfc0e06b56f72fdef0b4d83773325dfe9f62298
Chris Ford <christophertford@gmail.com>
2013-06-22 13:58:02 +0300

    Construct a zipper representing the maze.

--- src/legacygluefocustraversalpolicy/maze.clj
+++ src/legacygluefocustraversalpolicy/maze.clj
@@ -1,1 +1,12 @@
-(ns legacygluefocustraversalpolicy.maze)
+(ns legacygluefocustraversalpolicy.maze
+  (:require [clojure.zip :as zip]))
+
+(def random
+  (let [generator (java.util.Random. 42)]
+    (fn [] (-> generator (.nextInt Integer/MAX_VALUE)))))
+
+(def maze-zip
+  (partial zip/zipper (constantly true) :children #(assoc %1 :children %2))) 
+
+(defn ways [submaze]
+  (->> submaze zip/children (map :value)))
--- test/legacygluefocustraversalpolicy/test/maze.clj
+++ test/legacygluefocustraversalpolicy/test/maze.clj
@@ -1,3 +1,9 @@
 (ns legacygluefocustraversalpolicy.test.maze
   (:use midje.sweet 
         legacygluefocustraversalpolicy.maze))
+
+(fact "ways gets all the adjacent values."
+  (-> {:value 3 :children [{:value 4 :children []} {:value 44 :children []}]}
+      maze-zip
+      ways)
+  => [4 44])
